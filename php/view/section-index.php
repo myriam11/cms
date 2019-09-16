0703d8da7727a1df2e938b5637acac8b6cff5c4c
Myriam <myriamb2010@yahoo.fr>
2019-09-16 16:27:00 +0200

    commit 3

--- php/view/section-index.php
+++ php/view/section-index.php
@@ -1,3 +1,12 @@
 <section>
             <h3>Accueil</h3>
+
+
+            <div class="galerie">
+                <img src="assets/images/photo1.jpeg" alt="peinture" >
+                <img src="assets/images/photo2.jpeg" alt="eau" >
+                <img src="assets/images/photo3.jpeg" alt="gouter" >
+
+
+            </div>
         </section>
